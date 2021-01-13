6408d1c453b4e9f290b3d4d85d145697d4ab7f7e
Alina <alina12g@yahoo.com>
2021-01-13 11:02:09 +0100

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,8 @@
+ var firstNumber = prompt("What is your first number?");
+ var secondNumber = prompt("What is your second number?");
+ 
+ var sum = Number(firstNumber) + Number(secondNumber);
+ var multiply=  Number(firstNumber) * Number(secondNumber);
+ var divide =  Number(firstNumber) / Number(secondNumber);
+ var substract =  Number(firstNumber) - Number(secondNumber);
+ console.log(divide);
